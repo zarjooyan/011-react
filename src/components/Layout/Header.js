3511f757158be9369cf9001b902d8b75de75c531
zarjooyan <gaineraz@gmail.com>
2023-11-01 18:14:19 +0330

    feat: 010-react episode 10

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import classes from './Header.module.css'
+import HeaderCartButton from "./HeaderCartButton";
+
+import mealsImage from '../../assets/meals.jpg'
+
+const Header = (props) => {
+    return <>
+    <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+    </header>
+    <div className={classes['main-image']}>
+        <img src={mealsImage} alt="A table full of delicious food!"/>
+    </div>
+    </>
+};
+
+export default Header;
